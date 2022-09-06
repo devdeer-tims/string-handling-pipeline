a0a5cd84e50f7b2efb6f3795c9148d86fee8fc8e
devdeer-tims <tim.schneider@devdeer.com>
2022-09-06 13:48:26 +0200

    initial Transformer creation

---
+++ Logic/Logic.Transformers/RemoveItemTransformer.cs
@@ -0,0 +1,14 @@
+namespace devdeer.StringHandlingPipeline.Logic.Transformers
+{
+    using System.Collections.Generic;
+
+    using Core.BaseTypes;
+
+    public class RemoveItemTransformer : BaseTransformer
+    {
+        protected override void InternalTransform(string message, List<int> transformationIDs)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Logic/Logic.Transformers/ReverseTransformer.cs
@@ -0,0 +1,14 @@
+namespace devdeer.StringHandlingPipeline.Logic.Transformers
+{
+    using System.Collections.Generic;
+
+    using Core.BaseTypes;
+
+    public class ReverseTransformer : BaseTransformer
+    {
+        protected override void InternalTransform(string message, List<int> transformationIDs)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
