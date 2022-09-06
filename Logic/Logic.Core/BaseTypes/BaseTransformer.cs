01072922f55c30ea080841f62d2de1418665f8f9
devdeer-tims <tim.schneider@devdeer.com>
2022-09-06 13:16:59 +0200

    implemented BaseTransformer

---
+++ Logic/Logic.Core/BaseTypes/BaseTransformer.cs
@@ -0,0 +1,34 @@
+namespace devdeer.StringHandlingPipeline.Logic.Core.BaseTypes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Common.Interfaces;
+
+    /// <summary>
+    /// Abstract base class for all Transformers.
+    /// </summary>
+    public abstract class BaseTransformer : ITransformer
+    {
+        #region explicit interfaces
+
+        public void Transform(string message, List<int> transformationIDs)
+        {
+            InternalTransform(message, transformationIDs);
+        }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Must be overridden by children to handle the actual transform.
+        /// </summary>
+        /// <param name="message">The message you want to transform.</param>
+        /// <param name="transformationIDs">The provided transform-operations.</param>
+        protected abstract void InternalTransform(string message, List<int> transformationIDs);
+
+        #endregion
+    }
+}
