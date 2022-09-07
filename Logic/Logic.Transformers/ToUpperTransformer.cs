bbfc3fdd11269205ae3f4a0ef840cbba268cfbea
devdeer-tims <tim.schneider@devdeer.com>
2022-09-07 09:04:34 +0200

    basic implementation of ToUpperTransformer

--- Logic/Logic.Transformers/ToUpperTransformer.cs
+++ Logic/Logic.Transformers/ToUpperTransformer.cs
@@ -1,14 +1,29 @@
 namespace devdeer.StringHandlingPipeline.Logic.Transformers
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     using Core.BaseTypes;
 
+    /// <summary>
+    /// Implements the ToUpperTransformer.
+    /// </summary>
     public class ToUpperTransformer : BaseTransformer
     {
+        #region methods
+
+        /// <summary>
+        /// Transforms the givin message to all uppercase letters.
+        /// </summary>
+        /// <param name="message">The message you want to transform.</param>
+        /// <param name="transformationIDs">List of remaining Transformers for the <paramref name="message" />.</param>
         protected override void InternalTransform(string message, List<int> transformationIDs)
         {
-            throw new System.NotImplementedException();
+            CurrentMessage = message.ToUpper();
+            transformationIDs.Remove(0);
         }
+
+        #endregion
     }
 }
