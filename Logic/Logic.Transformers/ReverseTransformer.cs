69f97bea767bdc86b20cc157f0759bf725646200
devdeer-tims <tim.schneider@devdeer.com>
2022-09-07 09:04:59 +0200

    basic implementation of ReverseTransformer

--- Logic/Logic.Transformers/ReverseTransformer.cs
+++ Logic/Logic.Transformers/ReverseTransformer.cs
@@ -1,14 +1,24 @@
 namespace devdeer.StringHandlingPipeline.Logic.Transformers
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     using Core.BaseTypes;
-
+    /// <summary>
+    /// Implements the ReverseTransformer.
+    /// </summary>
     public class ReverseTransformer : BaseTransformer
     {
+        /// <summary>
+        /// Reverses the givin message.
+        /// </summary>
+        /// <param name="message">The message you want to transform.</param>
+        /// <param name="transformationIDs">List of remaining Transformers for the <paramref name="message" />.</param>
+
         protected override void InternalTransform(string message, List<int> transformationIDs)
         {
-            throw new System.NotImplementedException();
+            CurrentMessage = (string)message.Reverse();
+            transformationIDs.Remove(0);
         }
     }
 }
