aed1196aed616aeeb6dfd6b2edd23d5c65a58500
devdeer-tims <tim.schneider@devdeer.com>
2022-09-07 09:04:14 +0200

    basic implementation of ToLowerTransformer

--- Logic/Logic.Transformers/ToLowerTransformer.cs
+++ Logic/Logic.Transformers/ToLowerTransformer.cs
@@ -1,14 +1,29 @@
 namespace devdeer.StringHandlingPipeline.Logic.Transformers
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     using Core.BaseTypes;
 
+    /// <summary>
+    /// Implements the ToLowerTransformer.
+    /// </summary>
     public class ToLowerTransformer : BaseTransformer
     {
+        #region methods
+
+        /// <summary>
+        /// Transforms the givin message to all lowercase letters.
+        /// </summary>
+        /// <param name="message">The message you want to transform.</param>
+        /// <param name="transformationIDs">List of remaining Transformers for the <paramref name="message" />.</param>
         protected override void InternalTransform(string message, List<int> transformationIDs)
         {
-            throw new System.NotImplementedException();
+            CurrentMessage = message.ToLower();
+            transformationIDs.Remove(0);
         }
+
+        #endregion
     }
 }
